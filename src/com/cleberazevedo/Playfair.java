9d66a07cde638bda064dcef470432ae2ee9fa1a9
Cleber Evangelista Azevedo <cleberazevedo_@outlook.com>
2021-04-23 20:48:14 -0300

    Atividade da Disciplina SSI - 23abril21

---
+++ src/com/cleberazevedo/Playfair.java
@@ -0,0 +1,105 @@
+package com.cleberazevedo;
+
+
+import java.awt.Point;
+import java.util.Scanner;
+
+public class Playfair {
+    private static char[][] charTable;
+    private static Point[] positions;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String key = prompt("Entre com a chave: ", sc, 6);
+        String txt = prompt("Entre com a Mensagem: ", sc, 1);
+
+        createTable(key);
+
+        String enc = encode(prepareText(txt));
+
+        System.out.printf("%nMensagem Criptografada: %n%s%n", enc);
+        System.out.printf("%nMensagem Decriptografada: %n%s%n", decode(enc));
+    }
+
+    private static String prompt(String promptText, Scanner sc, int minLen) {
+        String s;
+        do {
+            System.out.print(promptText);
+            s = sc.nextLine().trim();
+        } while (s.length() < minLen);
+        return s;
+    }
+
+    private static String prepareText(String s) {
+        s = s.toUpperCase().replaceAll("[^A-Z]", "");
+        return s.replace("Q", "");
+    }
+
+    private static void createTable(String key) {
+        charTable = new char[5][5];
+        positions = new Point[26];
+
+        String s = prepareText(key + "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+
+        int len = s.length();
+        for (int i = 0, k = 0; i < len; i++) {
+            char c = s.charAt(i);
+            if (positions[c - 'A'] == null) {
+                charTable[k / 5][k % 5] = c;
+                positions[c - 'A'] = new Point(k % 5, k / 5);
+                k++;
+            }
+        }
+    }
+
+    private static String encode(String s) {
+        StringBuilder sb = new StringBuilder(s);
+
+        for (int i = 0; i < sb.length(); i += 2) {
+
+            if (i == sb.length() - 1)
+                sb.append(sb.length() % 2 == 1 ? 'X' : "");
+
+            else if (sb.charAt(i) == sb.charAt(i + 1))
+                sb.insert(i + 1, 'X');
+        }
+        return codec(sb, 1);
+    }
+
+    private static String decode(String s) {
+        return codec(new StringBuilder(s), 4);
+    }
+
+    private static String codec(StringBuilder text, int direction) {
+        int len = text.length();
+        for (int i = 0; i < len; i += 2) {
+            char a = text.charAt(i);
+            char b = text.charAt(i + 1);
+
+            int row1 = positions[a - 'A'].y;
+            int row2 = positions[b - 'A'].y;
+            int col1 = positions[a - 'A'].x;
+            int col2 = positions[b - 'A'].x;
+
+            if (row1 == row2) {
+                col1 = (col1 + direction) % 5;
+                col2 = (col2 + direction) % 5;
+
+            } else if (col1 == col2) {
+                row1 = (row1 + direction) % 5;
+                row2 = (row2 + direction) % 5;
+
+            } else {
+                int tmp = col1;
+                col1 = col2;
+                col2 = tmp;
+            }
+
+            text.setCharAt(i, charTable[row1][col1]);
+            text.setCharAt(i + 1, charTable[row2][col2]);
+        }
+        return text.toString();
+    }
+}
+
